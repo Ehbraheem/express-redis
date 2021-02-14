00dca51f9ebd3e81e8079185ddae2effe4ac53c4
Ehbraheem <ehbraheem@gmail.com>
2021-02-14 23:18:12 +0100

    Helper args and config

---
+++ src/utils/arguments.ts
@@ -0,0 +1,97 @@
+import Joi from 'joi';
+
+const allowedValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15] as const;
+
+type DBRange = typeof allowedValues[number];
+
+interface ConnectBase {
+  port?: number;
+  host?: string;
+}
+
+interface ConnectionReadyOption {
+  enableReadyCheck?: boolean;
+  enableOfflineQueue?: boolean;
+}
+export interface ConnectOption extends ConnectBase, ConnectionReadyOption {
+  family?: number;
+  password?: string;
+  db?: DBRange;
+  path?: string;
+  keepAlive?: number;
+  noDelay?: boolean;
+  connectionName?: string;
+  dropBufferSupport?: boolean;
+  connectTimeout?: number;
+  autoResubscribe?: boolean;
+  autoResendUnfulfilledCommands?: boolean;
+  lazyConnect?: boolean;
+  maxRetriesPerRequest?: number;
+  tls?: object;
+  keyPrefix?: string;
+  retryStrategy?: (times: number) => number | void;
+  reconnectOnError?: (error: Error) => boolean | 1 | 2;
+  readOnly?: boolean;
+  stringNumbers?: boolean;
+}
+
+export interface ConnectArgs extends ConnectBase {
+  options?: ConnectOption;
+}
+
+interface ClusterNode extends ConnectBase {
+  port: number;
+  host: string;
+  password?: string;
+}
+
+export type Dict = { [k: string]: ClusterNode };
+
+export interface ClusterOptions extends ConnectionReadyOption {
+  clusterRetryStrategy: (times: number, reason?: Error) => number;
+  dnsLookup?: (
+    hostname: string,
+    callback: (err: NodeJS.ErrnoException, address: string, family: number) => void
+  ) => void;
+  scaleReads: 'master' | 'slave' | 'all';
+  maxRedirections?: number;
+  retryDelayOnFailover?: number;
+  retryDelayOnClusterDown?: number;
+  retryDelayOnTryAgain?: number;
+  slotsRefreshTimeout?: number;
+  slotsRefreshInterval?: number;
+  redisOptions?: ConnectOption;
+  natMap: Dict;
+}
+export interface ClusterArgs {
+  nodes: ClusterNode[];
+  options?: ClusterOptions;
+}
+
+interface SentinelNode {
+  ip: string;
+  port: string;
+  prio?: number;
+}
+
+interface Sentinel extends ConnectBase {
+  port: number;
+  host: string;
+}
+export interface SentinelArgs {
+  name: string;
+  sentinels: Sentinel[];
+  sentinelPassword?: string;
+  role?: 'master' | 'slave';
+  preferredSlaves?: ((slaves: SentinelNode[]) => SentinelNode | null) | SentinelNode[];
+  sentinelRetryStrategy?: (times: number) => number;
+}
+
+export interface ConnectionArgs {
+  mode: 'sentinel' | 'cluster' | 'regular';
+  options: SentinelArgs | ClusterArgs | ConnectArgs | any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
+
+export interface SchemaArgs {
+  validate: Joi;
+}
---
+++ src/utils/schemas.ts
@@ -0,0 +1,110 @@
+import { ObjectSchema } from 'joi';
+import { SchemaArgs } from './arguments';
+
+const base = ({ validate }: SchemaArgs): object => ({
+  port: validate.number().default(6379),
+  host: validate.string().optional(),
+});
+
+const connectionReady = ({ validate }: SchemaArgs): object => ({
+  enableReadyCheck: validate.boolean().default(true),
+  enableOfflineQueue: validate.boolean().default(true),
+});
+
+const regularConnectOptionSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    ...base({ validate }),
+    ...connectionReady({ validate }),
+    family: validate.string().valid('4', '6').default('4'),
+    password: validate.string().optional(),
+    path: validate.string().optional(),
+    db: validate.number().min(0).max(15).default(0),
+    keepAlive: validate.number().default(100),
+    noDelay: validate.boolean().default(true),
+    connectionName: validate.string().optional(),
+    dropBufferSupport: validate.boolean().default(false),
+    connectTimeout: validate.number().default(10000).optional(),
+    autoResubscribe: validate.boolean().default(true),
+    autoResendUnfulfilledCommands: validate.boolean().default(true),
+    lazyConnect: validate.boolean().default(false),
+    maxRetriesPerRequest: validate.number().default(20).optional(),
+    tls: validate.object().keys({
+      ca: validate.binary(),
+    }),
+    keyPrefix: validate.string().optional().default(''),
+    retryStrategy: validate.func().optional(),
+    reconnectOnError: validate.func().optional(),
+    readOnly: validate.boolean().default(false),
+    stringNumbers: validate.boolean().default(false),
+  });
+
+export const regularConnectSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate
+    .object()
+    .keys({
+      ...base({ validate }),
+      options: regularConnectOptionSchema({ validate }),
+    })
+    .or('host', 'port', 'options');
+
+const clusterNode = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    ...base({ validate }),
+    password: validate.string().optional(),
+  });
+
+const clusterOptionsSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    clusterRetryStrategy: validate.func(),
+    dnsLookup: validate.func(),
+    scaleReads: validate.string().valid('master', 'slave', 'all').default('master'),
+    maxRedirections: validate.number().default(16),
+    retryDelayOnFailover: validate.number().default(100),
+    retryDelayOnClusterDown: validate.number().default(100),
+    retryDelayOnTryAgain: validate.number().default(100),
+    slotsRefreshTimeout: validate.number().default(1000),
+    slotsRefreshInterval: validate.number().default(5000),
+    redisOptions: regularConnectOptionSchema({ validate }),
+    ...connectionReady({ validate }),
+    natMap: validate.object().pattern(/[0-9:.]+/, clusterNode({ validate })),
+  });
+
+export const clusterConnectSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    options: clusterOptionsSchema({ validate }),
+    nodes: validate.array().items(clusterNode({ validate })).required(),
+  });
+
+const sentinelNodeSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    ip: validate.string().ip({
+      version: ['ipv4', 'ipv6'],
+    }),
+    port: validate.number().default(26379),
+    prio: validate.number().optional(),
+  });
+
+const sentinelSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    port: validate.string().default('26379'),
+    host: validate.string().optional(),
+  });
+
+export const sentinelConnectSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    name: validate.string(),
+    sentinels: validate.array().items(sentinelSchema({ validate })),
+    sentinelPassword: validate.string().optional(),
+    role: validate.string().valid('master', 'slave').default('master'),
+    preferredSlaves: validate
+      .alternatives()
+      .try(validate.func(), validate.array().items(sentinelNodeSchema({ validate })))
+      .optional(),
+    sentinelRetryStrategy: validate.func().optional(),
+  });
+
+export const subscriberSchema = ({ validate }: SchemaArgs): ObjectSchema =>
+  validate.object().keys({
+    subscribers: validate.func().required(),
+    topics: validate.array().items(validate.string().min(2)),
+  });
